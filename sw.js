66d141e3e051f5b9188447f03eeb694cd761fd87
aakiraakira <149947267+aakiraakira@users.noreply.github.com>
2025-07-26 18:29:32 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -15,7 +15,7 @@
 // --- 1. Configuration ---
 
 // IMPORTANT: Increment this version number every time you update the service worker file.
-const CACHE_NAME = 'reputifly-notes-cache-v60';
+const CACHE_NAME = 'reputifly-notes-cache-v61';
 
 // A comprehensive list of all files that make up the application's "shell".
 const APP_SHELL_URLS = [
